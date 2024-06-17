e6ab00c442080f0a1e770c3e2decb0b99ef837f0
2872109L <2872109L@student.gla.ac.uk>
2024-06-17 14:22:09 +0100

    uploading demo

---
+++ demo/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+# to import the view above
+from . import views
+
+# specific a variable (urlpatterns)
+# make it equal to a list
+# what it does is to call the views.home function when visting to base url of the website
+urlpatterns = [
+	# empty path "" = go to the base url of the website
+	# connect to views.home
+	path("", views.home, name="home"),
+    path("todos/", views.todos, name="todos")
+]
---
+++ demo/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
+# Create your views here.
+
+# simple hello world function
+# def home(request)
+#   return HttpResponse("Hello World!")
+
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
