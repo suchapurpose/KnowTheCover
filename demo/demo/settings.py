b93833e56de6d9024d7c371ee06de321fcd5007b
2872109L <2872109L@student.gla.ac.uk>
2024-07-31 23:54:44 +0100

    login app update

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -135,4 +135,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'leafletmap'
--- demo/users/urls.py
+++ demo/users/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_user, name="login"),
-  
+    path("logout", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    path("collections/", views.collections, name="collections"),
+]
